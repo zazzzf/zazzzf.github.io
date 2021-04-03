2e16fe614e5859799be35941db60d0bd3ee56be6
Zaz_zzf <40158049+zazzzf@users.noreply.github.com>
2021-04-03 20:26:23 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -19,4 +19,4 @@ git commit -m 'deploy'
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@gitee.com:zhaozhenfa1995/zhaozhenfa1995.git master
+git push -f git@github.com:zazzzf/zazzzf.github.io.git master
